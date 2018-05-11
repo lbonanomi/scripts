65d856ba8b7d10bce20bb2f768278a19c300c434
lbonanomi <lbonanomi@gmail.com>
2018-05-11 14:37:46 -0400

    Create active_directory_last_password_reset.py

---
+++ active_directory_last_password_reset.py
@@ -0,0 +1,36 @@
+#!/bin/python
+
+import ConfigParser
+from datetime import datetime, timedelta
+import ldap
+import sys
+
+# Simple config
+#
+config = ConfigParser.ConfigParser()
+config.read("~/.ssh/ldap_creds.py")   # Obviously a gloss.
+
+ldap_user = config.get("configuration","cn")
+ldap_password = config.get("configuration","password")
+
+ldap.set_option(ldap.OPT_REFERRALS, 0)
+
+basedn = "DC=activedirectory,DC=company,DC=com"
+searchFilter = "sAMAccountName=" + sys.argv[1]
+searchAttribute = ["pwdLastSet"]
+
+conn = ldap.initialize('ldap://activedirectory.company.com:389')
+conn.simple_bind_s(ldap_user, ldap_password)
+
+ldap_rez_id = conn.search(basedn, ldap.SCOPE_SUBTREE, searchFilter, searchAttribute)
+
+(lbl, data) = conn.result(ldap_rez_id, 0)
+(lbl, data) = data[0]
+
+last_reset = data['pwdLastSet'][0]
+
+last_reset_epoch = (int(last_reset) / 10000000) - timedelta(days=(1970 - 1601) * 365 + 89).total_seconds()
+
+print last_reset_epoch
+
+conn.unbind_s()
