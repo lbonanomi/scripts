6c6191b01a12331198851da07e24cd750c621d4c
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2019-04-28 10:22:20 -0400

    Update sniper.sh

--- sniper.sh
+++ sniper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Get plugin, twiddle enabled flag and dump to file /tmp/killer
-curl -s -k -u $SNIPER_USER:$SNIPER_PASSWORD https://$(hostname | sed -e 's/ny//' | tr -d '0').Employer.com/rest/plugins/1.0/$PLUGIN_KEY | sed -e 's/"enabled":true/"enabled":false/' > /tmp/killer;
-curl -s -k -u $SNIPER_USER:$SNIPER_PASSWORD -X PUT -H "Content-Type: application/vnd.atl.plugins.plugin+json" --data @/tmp/killer  https://$(hostname | sed -e 's/ny//' | tr -d '0').Employer.com/rest/plugins/1.0/$PLUGIN_KEY
+curl -s -k -u $SNIPER_USER:$SNIPER_PASSWORD https://jira.host.com/rest/plugins/1.0/$PLUGIN_KEY | sed -e 's/"enabled":true/"enabled":false/' > /tmp/killer;
+curl -s -k -u $SNIPER_USER:$SNIPER_PASSWORD -X PUT -H "Content-Type: application/vnd.atl.plugins.plugin+json" --data @/tmp/killer  https://jira.host.com/rest/plugins/1.0/$PLUGIN_KEY
